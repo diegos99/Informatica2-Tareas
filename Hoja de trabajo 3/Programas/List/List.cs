58b25da58e210648f65291592cd63b8c4302e4f8
Diego Vega <diegovr99@hotmail.com>
2018-02-18 23:05:06 -0600

    Se agrego la HT 3

---
+++ Hoja de trabajo 3/Programas/List/List.cs
@@ -0,0 +1,10 @@
+abstract class Lista<T> : IList<T>
+{
+    public T elemento{get; private set;}
+    
+    public abstract int Length { get; }
+
+    public abstract bool Get(int indice);
+    public abstract void Push(T elemento);
+    public abstract bool Set(int indice, T elemento);
+}
